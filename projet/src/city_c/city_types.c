59682e57f2e835aecc2c36be299c9b3d688a4598
Alexandre LY <alexandrely98@yahoo.com>
2022-12-18 01:02:05 +0100

    update

--- projet/src/city_c/city_types.c
+++
@@ -1,9 +0,0 @@
-/* --- Generated the 18/12/2022 at 0:54 --- */
-/* --- heptagon compiler, version 1.05.00 (compiled thu. dec. 15 17:28:19 CET 2022) --- */
-/* --- Command line: /home/a/.opam/default/bin/heptc -c -target c city.ept --- */
-
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include "city_types.h"
-
