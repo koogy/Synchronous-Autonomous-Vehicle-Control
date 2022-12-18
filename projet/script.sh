4da9228fcc8a1a629ade7082fe8767aeb60cf2b0
Alexandre LY <alexandrely98@yahoo.com>
2022-12-18 18:49:21 +0100

    script

--- projet/script.sh
+++ projet/script.sh
@@ -1,3 +1,12 @@
 #!/bin/bash 
 
-gnome-terminal -- ./scontest assets/10.map
+gnome-terminal -- ./scontest assets/00.map
+gnome-terminal -- ./scontest assets/01.map
+gnome-terminal -- ./scontest assets/02.map
+gnome-terminal -- ./scontest assets/03.map
+gnome-terminal -- ./scontest assets/04.map
+gnome-terminal -- ./scontest assets/05.map
+gnome-terminal -- ./scontest assets/06.map
+gnome-terminal -- ./scontest assets/07.map
+gnome-terminal -- ./scontest assets/08.map
+gnome-terminal -- ./scontest assets/09.map
